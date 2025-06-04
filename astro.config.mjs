d2cf107a733251955fe55482ed88b54fa904fbcf
MosleyTheMalO <93835010+MosleyTheMalO@users.noreply.github.com>
2025-06-03 23:41:08 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://username.github.io", // Replace 'username' with your GitHub username
+  site: "https://splurt-station.github.io", // Replace 'username' with your GitHub username
   base: "/splurt-webpage", // Replace with your repository name if different
   vite: {
     plugins: [tailwindcss()],
